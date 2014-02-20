fb29a2a02257ed4df0ce1c62533e78f99b185c48
student <student@student-virtual-machine>
2014-02-20 16:02:25 +0200

    Almost Final

---
+++ src/main/java/com/tpg200s/cput/program1/service/testProgram1ServiceImpl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tpg200s.cput.program1.service;
+
+/**
+ *
+ * @author student
+ */
+public class testProgram1ServiceImpl implements program1Service
+{
+
+    public int add(int a, int b) {
+      return a + b;
+    }
+    
+    
+    
+    
+    
+}
